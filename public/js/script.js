a9fc7a8f2c2615eb2f3db8d38a127dd1c8080559
Malavika Nair <malavika03@outlook.com>
2023-07-14 19:12:56 +0530

    Intial commit

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    const searchBtn = document.querySelectorAll('.searchBtn');
+    const searchBar = document.querySelector('.searchBar');
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = document.getElementById('searchTerm')
+
+
+    for (var i =0; i < searchBtn.length; i++)
+    {
+        searchBtn[i].addEventListener('click', function(){
+            searchBar.style.visibility ='visible';
+            searchBar.classList.add('open');
+            this.setAttribute('aria-expanded', 'true');
+            searchInput.focus();
+        });
+    }
+
+    
+    searchClose.addEventListener('click', function(){
+            searchBar.style.visibility ='hidden';
+            searchBar.classList.remove('open');
+            this.setAttribute('aria-expanded', 'false');
+            
+        });
+    
+
+});
